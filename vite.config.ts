507f3971e7c4b61aa0cd5d34564a74e30e77a998
WillGarp <williammarcellowm@gmail.com>
2026-08-21 14:05:31 -0300

    Corrige deploy do GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: "./",
+  base: process.env.GITHUB_ACTIONS ? "/reflora/" : "./",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
